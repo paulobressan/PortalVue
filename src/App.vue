0d93ebc7de792a5e20f4210346d1df9252a61e83
Paulo Bressan <paulo.bressan>
2018-06-11 14:32:44 -0300

    Adicionando o componenten painel.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,68 @@
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <table>
-      <thead>
-        <th>Imagem</th>
-        <th>Nome</th>
-      </thead>
-      <tbody>
-        <tr v-for="produto in produtos">       
-          <td><img :src="produto.url" :alt="produto.nome" style=""></td>
-          <td>{{produto.nome}}</td>
-        </tr>
-      </tbody>
-    </table>
+<!-- template ou html, parte visual do componente -->
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+    <ul class="lista-produtos">
+      <li class="lista-produtos-item" v-for="produto in produtos">
+        <meu-painel :titulo="produto.nome">
+          <figure>             
+            <img :src="produto.url" :alt="produto.nome">
+          </figure>
+        </meu-painel>      
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+
+//importando outros componentes
+import Painel from './components/shared/painel/Painel.vue';
+
+
+//dados do componente
 export default {
+  //Propriedade components para usar outros componentes, desde que estaja importado.
+  components: {
+    'meu-painel': Painel
+  },
   name: "app",
   data() {
+    //dados ou objeto do componente.
     return {
       titulo: "Produtos",
       produtos: []
     };
   },
-  created(){
-    this.$http.get("http://localhost:5000/api/product")
+  created() {
+    this.$http
+      .get("http://localhost:5000/api/product")
       .then(res => res.json())
-      .then(produtos => this.produtos = produtos);
+      .then(produtos => (this.produtos = produtos));
   }
 };
 </script>
 
 <style>
-  img{
-    width: 50px;
-  }
+/* Estilo do componente */
+img {
+  width: 100%;
+}
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-produtos {
+  list-style: none;
+}
+
+.lista-produtos-item {
+  display: inline-block;
+}
 </style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,47 @@
+<template>
+<!-- template ou html, parte visual do componente -->
+    <div class="painel">
+        <h2 class="painel-titulo">{{titulo}}</h2>
+        <!-- Slot é para definir onde o conteudo passado de outros componente vai ser exibido. -->
+        <slot class="painel-conteudo"></slot>
+    </div>
+</template>
+
+<script>
+//dados do componente
+export default {
+    //aplicando props para receber dados de outros componentes
+    props: ['titulo']
+};
+</script>
+
+<style scoped>
+/* O scoped torna os estilos aplicavel somente para o componente "Painel" */
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+*  {
+    box-shadow: 5px 5px 10px black;
+}
+</style>
